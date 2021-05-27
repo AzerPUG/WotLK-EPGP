35f8f138563ac077b58671c29ab41f0a6aa987d6
Arco Dielhof <arcodielhof@hotmail.com>
2021-05-27 18:21:00 +0200

    Version++;

--- EPGP.lua
+++ EPGP.lua
@@ -1,6 +1,6 @@
 if TBCEPGP == nil then TBCEPGP = {} end
 TBCEPGP.events = {}
-TBCEPGP.Version = 5
+TBCEPGP.Version = 6
 local AddOnName = "TBC-EPGP"
 
 local UpdateFrame, EventFrame, EPGPUserFrame, scrollPanel = nil, nil, nil, nil
